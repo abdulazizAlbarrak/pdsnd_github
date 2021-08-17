6303bdfa178dc9dd67bb5b5bdc96306c50e831b0
DESKTOP-E0165ER <tabdulazizt@gmail.com>
2021-08-17 04:53:31 +0300

    refactor: updating the welcome massage to the bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -17,16 +17,16 @@ def get_filters():
 
 
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('welcome! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     city_list = ['chicago','new york city','washington']
     month_list = ['all','january','february','march','april','may','june','july','august','september','october','november','december']
     day_list = ['all','monday','tuesday','wednesday','thursday','friday','saturday','sunday']
-    
+
     city = ""
     month = ""
     day = ""
-    
+
     while True:
         city = input('enter the city name: ')
         if str(city).lower() in city_list:
